b4ff461116369cd0021122131f872fe6049ea989
G00394619@atu.ie <g00394619@atu.ie>
2023-10-04 11:10:50 +0100

    Added UserController

--- src/main/java/ie/atu/iocdi/UserController.java
+++ src/main/java/ie/atu/iocdi/UserController.java
@@ -1,4 +1,15 @@
 package ie.atu.iocdi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+    private final UserService  userService;
+
+    @Autowired
+    public UserController (UserService userService){
+        this.userService = userService;
+    }
+
 }
